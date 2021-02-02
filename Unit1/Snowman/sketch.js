597943f0fec54d95fa4ba3af030abe5291a9bdad
magneticscenes <72484476+magneticscenes@users.noreply.github.com>
2021-02-02 16:48:18 -0600

    Added mp1 and fonts

---
+++ Unit1/Snowman/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background ('yellow')
+  ellipse(width/2, height/2 -75, 70, 70);
+ellipse(width/2, height/2, 100, 100);
+ellipse(width/2, height/2 +75, 150, 150);
+
+}
