435ff26594abc92192ab0c7089e43109d92c1757
magneticscenes <72484476+magneticscenes@users.noreply.github.com>
2021-02-02 17:04:06 -0600

    completed fonts program

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,7 +1,22 @@
+let f1, f2 ;
+
 function setup() {
   createCanvas(500, 500);
+  textAlign(CENTER);
+
+f1 = loadFont("assets/bladesinger.ttf");
+f2 = loadFont("assets/cutelittlesheep.ttf");
+f3 = loadFont("assets/SilentLandfield.ttf");
 }
 
 function draw() {
+background(100);
+textSize(48);
 
+textFont(f1);
+text("Join my", width/2, height/2);
+textFont(f2, 64);
+text("World of Warcraft", width/2, height/2 + 60);
+textFont(f3);
+text("Guild", width/2, height/2 + 120);
 }
