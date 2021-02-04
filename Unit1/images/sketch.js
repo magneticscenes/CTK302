0d99f14bbb26b271db746aba53d5c667462c2437
magneticscenes <72484476+magneticscenes@users.noreply.github.com>
2021-02-04 00:37:36 -0600

    fixed images

--- Unit1/images/sketch.js
+++ Unit1/images/sketch.js
@@ -3,11 +3,12 @@ let night;
 
 function setup() {
   createCanvas(800, 800);
-
+day = loadImage("assets/day.jpg");
+night = loadImage("assets/night.jpg");
 imageMode(CENTER);
 }
 
 function draw() {
-image(day, width/2, height/2, 200, 200);
-image(night, width/2, height/2 -200, 200, 200);
+image(day, width/2, height/2, 300, 200);
+image(night, width/2, height/2 -200, 300, 200);
 }
