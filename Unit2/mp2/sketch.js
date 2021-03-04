c40dee23ddcf88417b19c7e24fd6c2688a3c3b61
magneticscenes <72484476+magneticscenes@users.noreply.github.com>
2021-03-03 21:45:35 -0600

    completed I guess

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -1,16 +1,41 @@
-//let garden1;
 let WateringCan;
+let MidPlant;
+let Pot;
+let Room;
+let Monster;
+let Vines;
+let Start;
+let Here;
+let Baby;
+let ClickTake;
+let Wait;
+let Click;
+let laugh;
+let y = 0;
 let state = 0;
-let myTimer = 0 ;
+let myTimer = 0;
+
+function preload() {
+  laugh = loadSound("assets/plantlaugh.mp3");
+}
 
 function setup() {
+
   createCanvas(800, 600);
 
-
-  // garden1 = loadImage("assets/garden1.jpeg");
   WateringCan = loadImage("assets/WateringCan.png");
+  MidPlant = loadImage("assets/MidPlant.png");
+  Pot = loadImage("assets/Pot.png");
+  Room = loadImage("assets/Room.png");
+  Monster = loadImage("assets/Monster.png");
+  Vines = loadImage("assets/Vines.png");
+  Start = loadImage("assets/Start.png");
+  Here = loadImage("assets/HereSeed.png");
+  Baby = loadImage("assets/BabyPlant.png");
+  ClickTake = loadImage("assets/ClickTake.png");
+  Wait = loadImage("assets/Wait.png");
+  Click = loadImage("assets/Click.png");
   imageMode(CENTER);
-
 }
 
 function draw() {
@@ -19,65 +44,92 @@ function draw() {
 
     //THIS IS STARTING SCREEN
     case 0:
-      // image(garden1, 100, 100);
-      text("Click to start", 100, 100);
-
+      laugh.stop();
+      image(Start, width / 2, height / 2);
       break;
-//THIS IS SEED STAGE
-    case 1:
-      background(100);
-      text("its a seed", 100, 100);
-      //image(garden1, 100, 100);
 
+      //GIVEN SEED
+    case 1:
+      image(Room, width / 2, height / 2);
+      image(Pot, width / 2 - 14, height / 2 + 225);
+      image(Here, width / 2 + 30, height / 2 - 30);
+      image(ClickTake, width / 2 + 230, height / 2 + 200);
+      break;
+
+      //THIS TINY PLANT
+    case 2:
+      image(Room, width / 2, height / 2);
+      image(Baby, width / 2 - 14, height / 2 + 178);
+      image(Click, width / 2 + 250, height / 2 + 250);
       break;
 
       //THIS IS WATERING CUE
-    case 2:
-      background('pink');
-      text("youre watering", 100, 100);
-      image(WateringCan, width/2, height/2);
-myTimer++ ;
-if (myTimer > 3*60){
- myTimer = 0 ;
- state = 3 ;
-}
+    case 3:
+      image(Room, width / 2, height / 2);
+      image(Wait, width / 2 + 250, height / 2 + 200);
+      image(WateringCan, width / 2 + 50, height / 2);
+      image(Baby, width / 2 - 14, height / 2 + 178);
 
+      myTimer++;
+      if (myTimer > 3 * 60) {
+        myTimer = 0;
+        state = 4;
+      }
       break;
 
       //THIS IS MEDIUM PLANT
-      case 3:
-      background('orange');
-      text("you did it now its a plant", 100, 100);
-      //image(garden1, 100, 100);
+    case 4:
+      image(Room, width / 2, height / 2);
+      image(MidPlant, width / 2, height / 2 + 150);
+      image(Click, width / 2 + 250, height / 2 + 250);
       break;
 
-            //THIS IS WATERING CUE
-    case 4:
-      background('pink');
-      text("youre watering", 100, 100);
-      //image(garden1, 100, 100);
-myTimer++ ;
-if (myTimer > 3*60){
- myTimer = 0 ;
- state = 5 ;
-}
+      //THIS IS WATERING CUE
+    case 5:
+      image(Room, width / 2, height / 2);
+      image(Wait, width / 2 + 250, height / 2 + 200);
+      image(MidPlant, width / 2, height / 2 + 150);
+      image(WateringCan, width / 2 + 50, height / 2);
+      image(Pot, width / 2 - 14, height / 2 + 225);
 
+      myTimer++;
+      if (myTimer > 3 * 60) {
+        myTimer = 0;
+        state = 6;
+      }
       break;
 
       //THIS IS MONSTER PLANT
-      case 5:
-      background('red');
-      text("monster", 100, 100);
-      //image(garden1, 100, 100);
+    case 6:
+      laugh.play();
+      image(Room, width / 2, height / 2);
+      image(Monster, width / 2 + 16, height / 2 + 4);
+
+      push();
+      translate(0, y);
+      avatar();
+      pop();
+      y = y - 2;
+      if (y < -500) {
+        y = 0
+        y = y - 0;
+      }
+
+      function avatar() {
+        image(Vines, width / 2, height / 2 + 500);
+      }
+      background(255, 0, 20, 50);
+      myTimer++;
+      if (myTimer > 8 * 60) {
+        myTimer = 0;
+        state = 0;
+      }
       break;
   }
 }
-
-  function mouseReleased() {
-    state++;
-
-    // if (state > 5) {
-    //  state = 0
-    // }
-
+function mouseReleased() {
+  state++;
+}
+function touchStarted() {
+  getAudioContext().resume();
 }
