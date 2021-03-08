c0dd3f74c0455e5da29750e9ec0b510be1db288e
magneticscenes <72484476+magneticscenes@users.noreply.github.com>
2021-03-07 19:50:56 -0600

    multsound

---
+++ Unit2/multsound/sketch.js
@@ -0,0 +1,58 @@
+let state = 0;
+let cat, dog, woo;
+
+function preload(){
+cat = loadSound("assets/cat.mp3");
+dog = loadSound("assets/dog.mp3");
+woo = loadSound("assets/woo.mp3");
+}
+function setup() {
+  createCanvas(500, 500);
+
+  cat.loop();
+  cat.pause();
+  dog.loop();
+  dog.pause();
+  woo.loop();
+  woo.pause();
+}
+
+function draw() {
+
+
+  switch (state) {
+    case 0:
+cat.play();
+state = 1;
+      break;
+
+    case 1:
+    text("Listen to Song 1.". width/2, height/2);
+
+      break;
+
+    case 2:
+woo.play();
+state = 3;
+      break;
+
+      case 3:
+  text("Listen to Song 2.". width/2, height/2);
+        break;
+
+        case 4:
+    dog.play();
+    state = 5;
+          break;
+
+          case 5:
+  text("Listen to Song 2.". width/2, height/2);
+            break;
+}
+function mouseReleased() {
+  state++;
+
+  if (state > 5) {
+    state = 0
+  }
+}
