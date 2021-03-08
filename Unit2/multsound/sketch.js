d446c9db41c39bf10b3f33254de199d29c6b3126
magneticscenes <72484476+magneticscenes@users.noreply.github.com>
2021-03-07 20:08:38 -0600

    finish sounds

--- Unit2/multsound/sketch.js
+++ Unit2/multsound/sketch.js
@@ -1,13 +1,10 @@
 let state = 0;
 let cat, dog, woo;
 
-function preload(){
-cat = loadSound("assets/cat.mp3");
-dog = loadSound("assets/dog.mp3");
-woo = loadSound("assets/woo.mp3");
-}
-function setup() {
-  createCanvas(500, 500);
+function preload() {
+  cat = loadSound("assets/cat.mp3");
+  dog = loadSound("assets/dog.mp3");
+  woo = loadSound("assets/woo.mp3");
 
   cat.loop();
   cat.pause();
@@ -17,42 +14,52 @@ function setup() {
   woo.pause();
 }
 
-function draw() {
+function setup() {
+  createCanvas(500, 500);
 
+}
+
+function draw() {
+  background('grey');
 
   switch (state) {
     case 0:
-cat.play();
-state = 1;
+      cat.play();
+      state = 1;
       break;
 
     case 1:
-    text("Listen to Song 1.". width/2, height/2);
-
+      text("Listen to Song 1", width / 2, height / 2);
       break;
 
     case 2:
-woo.play();
-state = 3;
+      woo.play();
+      state = 3;
       break;
 
-      case 3:
-  text("Listen to Song 2.". width/2, height/2);
-        break;
+    case 3:
+      text("Listen to Song 2", width / 2, height / 2);
+      break;
 
-        case 4:
-    dog.play();
-    state = 5;
-          break;
+    case 4:
+      dog.play();
+      state = 5;
+      break;
 
-          case 5:
-  text("Listen to Song 2.". width/2, height/2);
-            break;
-}
-function mouseReleased() {
-  state++;
-
-  if (state > 5) {
-    state = 0
+    case 5:
+      text("Listen to Song 3", width / 2, height / 2);
+      break;
   }
+}
+
+function mouseReleased() {
+
+  cat.pause();
+  dog.pause();
+  woo.pause();
+
+  state++;
+  if (state > 5) state = 0;
+
+
 }
