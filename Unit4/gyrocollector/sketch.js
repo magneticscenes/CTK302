2b6815f8c0c87c1f85911f488e2aa571720f5c14
magneticscenes <72484476+magneticscenes@users.noreply.github.com>
2021-04-30 21:10:42 -0500

    hitboxes again

--- Unit4/gyrocollector/sketch.js
+++ Unit4/gyrocollector/sketch.js
@@ -150,7 +150,7 @@ image(garden, width/2, height/2);
   for (var i = 0; i < cars.length; i++) {
     cars[i].display();
     cars[i].drive();
-    if (cars[i].pos.dist(frogPos) < 10) {
+    if (cars[i].pos.dist(frogPos) < 30) {
       cars.splice(i, 1);
     }
   }
